a4bc5f8ab1ea07fb0d93cdd1c3c4ef8d316b1a35
vitormedeiros145 <163922400+vitormedeiros145@users.noreply.github.com>
2024-05-04 00:20:47 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+// Array de fotos
+const photos = ['photos/DSC_9568-1.JPG']; // Adicione o caminho completo das fotos aqui
+
+// Função para exibir fotos na galeria
+function displayPhotos() {
+    const gallery = document.getElementById('gallery');
+    photos.forEach(photo => {
+        const img = document.createElement('img');
+        img.src = photo; // Aqui atribuímos o caminho de cada foto ao atributo src do elemento de imagem
+        img.alt = 'Fotografia';
+        img.classList.add('gallery-image')
+        gallery.appendChild(img);
+    });
+}
+
+// Chame a função para exibir fotos na galeria
+displayPhotos();
+
+// Função para lidar com o envio do formulário de contato
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    
+    
+    const formData = new FormData(this);
+    for (var pair of formData.entries()) {
+        console.log(pair[0] + ': ' + pair[1]);
+    }
+    alert('Mensagem enviada com sucesso!');
+});
+
+// Função para lidar com o envio do formulário de agendamento de sessão fotográfica
+document.getElementById('booking-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+   
+    
+    const formData = new FormData(this);
+    for (var pair of formData.entries()) {
+        console.log(pair[0] + ': ' + pair[1]);
+    }
+    alert('Solicitação de agendamento enviada com sucesso!');
+});
+
+// Chamar a função para exibir as fotos na galeria quando a página carregar
+window.onload = displayPhotos;
